860b0d821229e594f7e6d7eae1974933d63bc053
Martin Pratt <martinpratt@Martins-MacBook-Pro.local>
2020-05-18 09:42:11 -0400

    New Assets

---
+++ Assets/Scripts/AzureRestTesting.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Net;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class AzureRestTesting : MonoBehaviour
+{
+    // Start is called before the first frame update
+    async void Start()
+    {
+        StartCoroutine(GetMetadata());
+        //ListBlobsAnonymously();
+	}
+
+    //async void ListBlobsAnonymously()
+    //{
+    //    // Get a reference to a container that's available for anonymous access.
+    //    CloudBlobContainer container = new CloudBlobContainer(
+    //        new Uri(@"https://haringerverdiag.blob.core.windows.net/ios/geoxplorer-outcrop"));
+
+    //    var blobs = await container.ListBlobsSegmentedAsync();
+
+    //    // Note this is only possible when the container supports full public read access.
+    //    // List blobs in the container.
+    //    foreach (IListBlobItem blobItem in )
+    //    {
+    //        print(blobItem.Uri);
+    //    }
+    //}
+
+    IEnumerator GetMetadata()
+    { 
+        DateTime now = DateTime.UtcNow;
+        
+
+
+        var request = WebRequest.CreateHttp("https://haringerverdiag.blob.core.windows.net/ios/geoxplorer-outcrop?restype=container&comp=list");
+        request.Method = "GET";
+        request.Credentials = CredentialCache.DefaultCredentials;
+        request.Headers.Add("x-ms-date", now.ToString("R", CultureInfo.InvariantCulture));
+        //request.Headers.Add("x-ms-version", "2017-07-29");
+        //request.Headers.Add("Authorization", "qNTIyFtXLm6W9+LV5fkVDeazdHvQVYJI+MYsngLN8kuUrlu8iqlQfTw52fKhTMExE4c2kR/eMS1k988j7pp5Xg==");
+
+        //httpRequestMessage.Headers.Authorization = AzureStorageAuthenticationHelper.GetAuthorizationHeader(storageAccountName, storageAccountKey, now, httpRequestMessage);
+
+        // Get the response.  
+        WebResponse response = request.GetResponse();
+        yield return response;
+        print(((HttpWebResponse)response).StatusDescription);
+        response.Close();
+    }
+}
---
+++ Assets/Scripts/FirebaseTesting.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using Newtonsoft.Json;
+using System.Net;
+using System.Text;
+using UnityEngine.Networking;
+
+public class FirebaseTesting : MonoBehaviour
+{
+    /// <summary>
+    /// Gets current stored anchor information, downloads, checks for expiration.
+    /// On creating a new ID, checks for conflicting name and uploads to Firebase.
+    /// </summary>
+
+
+    //Private variables
+    List<AzureSpatialAnchorObject> anchorObjects = new List<AzureSpatialAnchorObject>();
+    bool conflictFound;
+
+    //Public variables
+    public string anchorName { get; set; } //this is set by the UI Input Field
+
+    // Initial settings
+    void Start()
+    {
+        conflictFound = false;
+        StartCoroutine(FetchCurrentAnchors());
+    }
+
+    //Script to put (overwrite) the anchor selection on Firebase
+    public void PutAnchors()
+    {
+        AzureSpatialAnchorObject anchorObject = new AzureSpatialAnchorObject();
+        anchorObject.name = anchorName;
+        anchorObject.identifier = Guid.NewGuid().ToString(); //This will be the ASA identifier
+        anchorObject.dateCreated = DateTime.Now;
+        anchorObject.dateExpired = DateTime.Now.AddDays(7); //this is the time set by the ASA expiration
+
+        //Check if name has already been taken by a stored anchor
+        foreach (var anchor in anchorObjects)
+        {
+            if (anchor.name == anchorObject.name)
+            {
+                conflictFound = true;
+                print("Anchor name is already used, please pick another");
+            }
+        }
+
+        //Upload the information to Firebase using PUT
+        if (!conflictFound)
+        {
+            anchorObjects.Add(anchorObject);
+            var json = JsonConvert.SerializeObject(anchorObjects);
+
+            var request = WebRequest.CreateHttp("https://flasasharing.firebaseio.com/anchors.json");
+            request.Method = "PUT";
+            request.ContentType = "application/json";
+            var buffer = Encoding.UTF8.GetBytes(json);
+            request.ContentLength = buffer.Length;
+            request.GetRequestStream().Write(buffer, 0, buffer.Length);
+        }
+
+        conflictFound = false;
+    }
+
+    //Finds anchor name in stored anchor list
+    public void FindAnchorByName()
+    {
+        foreach (var anchor in anchorObjects)
+        {
+            if (anchor.name == anchorName)
+            {
+                print("Found " + anchor.name + ": " + anchor.identifier);
+            }
+        }
+    }
+
+    //Fetches the current list of anchor information on Firebase
+    public IEnumerator FetchCurrentAnchors()
+    {
+        conflictFound = false;
+        var uwr = UnityWebRequest.Get("https://flasasharing.firebaseio.com/anchors.json");
+        yield return uwr.SendWebRequest();
+
+        //Continue if there are anchors stored, otherwise there's no point doing any more
+        if(uwr.downloadHandler.text != "null")
+        { 
+            List<AzureSpatialAnchorObject> downloadedAnchors = JsonConvert.DeserializeObject<List<AzureSpatialAnchorObject>>(uwr.downloadHandler.text);
+            print(downloadedAnchors.Count + "Anchors Stored on Firebase");
+            foreach (var anchor in downloadedAnchors)
+            {
+                //Check if anchor has expired - if it has it's not added to the anchorObjects list and so when a new list is uploaded it won't be included
+                if (anchor.dateExpired > DateTime.Now)
+                {
+                    anchorObjects.Add(anchor);
+                }
+            }
+        }
+    }
+
+    //Anchor class
+    public class AzureSpatialAnchorObject
+    {
+        public string name { get; set; }
+        public string identifier { get; set; }
+        public DateTime dateCreated { get; set; }
+        public DateTime dateExpired { get; set; }
+    }
+}
